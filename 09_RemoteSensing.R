bb57ee232887a8fc477c3bd8d85d86b6cea09f71
Adam M. Wilson <adammichaelwilson@gmail.com>
2016-11-13 23:03:24 -0500

    Update Modis Presentation

--- 09_RemoteSensing.R
+++ 09_RemoteSensing.R
@@ -3,6 +3,11 @@
 #' ---
 #' 
 #' 
+#' <div>
+#' <iframe src="09_presentation/09_RemoteSensing.html" width="100%" height="600px"> </iframe>
+#' </div>
+#' 
+#' 
 #' [<i class="fa fa-file-code-o fa-3x" aria-hidden="true"></i> The R Script associated with this page is available here](`r #output`).  Download this file and open it (or copy-paste into a new script) with RStudio so you can follow along.  
 #' 
 #' 
